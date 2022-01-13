1c7fa48ff3c899f7132b9393bd8496c249cca298
Smit Vavliya <srvavliya79@gmail.com>
2022-01-13 22:35:39 +0530

    c

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -29,7 +29,7 @@ class App extends Component {
         totalPages: data.totalPages,
         totalElements: data.totalElements
       });
-    }, 100);
+    }, 3000);
   }
 
   setUpdatedData = (data) => {
